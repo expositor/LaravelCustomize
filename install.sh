f29e115468a4331b395de6a9c53dffc8cf43244b
expositor <expositor@users.noreply.github.com>
2014-03-02 03:29:17 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ curl -L -o index.php https://raw.github.com/expositor/LaravelCustomize/master/in
 curl -L -o paths.php https://raw.github.com/expositor/LaravelCustomize/master/paths.php
 curl -L -o database.php https://raw.github.com/expositor/LaravelCustomize/master/database.php
 curl -L -o remote.php https://raw.github.com/expositor/LaravelCustomize/master/remote.php
+
+mv index.php /vagrant/laravel/public/
+mv paths.php /vagrant/laravel/bootstrap/
+mv database.php /vagrant/laravel/app/config/
+mv remote.php /vagrant/laravel/app/config/
