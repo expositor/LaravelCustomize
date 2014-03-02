76c6c4afb38c553ec4a348ddbb2b7129009e9210
expositor <lee4runner@hotmail.com>
2014-03-02 02:46:19 -0800

    more stuff

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Replace with Custom Configuration
+curl -L -o index.php https://raw.github.com/expositor/LaravelCustomize/master/index.php
+curl -L -o paths.php https://raw.github.com/expositor/LaravelCustomize/master/paths.php
+curl -L -o database.php https://raw.github.com/expositor/LaravelCustomize/master/database.php
+curl -L -o remote.php https://raw.github.com/expositor/LaravelCustomize/master/remote.php
